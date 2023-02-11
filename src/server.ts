fc540c3fcb8e6d1c82c5175a1681a87ace7cc5ff
Êxodo Gurgel <exodowellis@gmail.com>
2023-02-10 22:38:47 -0300

    :sparkles: feat: Add and treat env with zod

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,24 @@
 import fastify from 'fastify'
+import { randomUUID } from 'node:crypto'
+import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
+app.get('/hello', async () => {
+  const transaction = await knex('transactions')
+    .insert({
+      id: randomUUID(),
+      title: 'Transação de teste',
+      amount: 1000,
+    })
+    .returning('*')
+
+  return transaction
+})
+
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => console.log('HTTP Server Running'))
